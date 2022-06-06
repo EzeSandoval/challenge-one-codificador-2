c52ac08f42910353ab16e5659d36f5d4b86d62d5
ezequiel sandoval <elezesandoval2@gmail.com>
2022-06-06 16:42:08 -0300

    eliminamos la carpeta img

---
+++ js/principal.js
@@ -0,0 +1,39 @@
+var botonEncriptar = document.querySelector(".bt-encriptar"); 
+var botonDesencriptar = document.querySelector(".bt-desencriptar");
+var botonCopiar = document.querySelector(".bt-copiar");
+
+botonEncriptar.addEventListener("click", function(){
+    encriptar();
+    borrarMensaje();
+});
+botonDesencriptar.addEventListener("click", function(){
+    desencriptar();
+    borrarMensaje();
+});
+    
+botonCopiar.addEventListener("click", function(){
+   copiarTexto();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
