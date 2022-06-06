d22a3ffc4180112c3648214f1aaf7dd7ccb06b4e
ezequiel sandoval <elezesandoval2@gmail.com>
2022-06-06 17:08:55 -0300

    terminamos css

---
+++ js/funciones.js
@@ -0,0 +1,32 @@
+function encriptar(){
+    var texto = document.querySelector("#textoArea").value; 
+    var textoAreaEncriptar = document.querySelector("#textoEncriptado");
+    var textoEncriptado = texto.replace(/e/ig, "enter")
+    .replace(/i/ig, "imes")
+    .replace(/a/ig, "ai")
+    .replace(/o/ig, "over")
+    .replace(/u/ig, "ufat");
+    return textoAreaEncriptar.innerHTML = textoEncriptado;  
+}
+
+function desencriptar(){
+    var texto = document.querySelector("#textoArea").value;  
+    var textoAreaEncriptar = document.querySelector("#textoEncriptado");
+    var textoEncriptado = texto.replace(/enter/ig, "e")
+    .replace(/imes/ig, "i")
+    .replace(/ai/ig, "a")
+    .replace(/over/ig, "o")
+    .replace(/ufat/ig, "u");
+    return textoAreaEncriptar.innerHTML = textoEncriptado;
+}
+
+function borrarMensaje(){
+    var texto = document.querySelector("#textoArea");
+    texto.value = "";
+}
+
+function copiarTexto(){ 
+    var areaTexto = document.getElementById("textoEncriptado");
+    areaTexto.select();
+    document.execCommand("copy");
+}
---
+++ js/principal.js
@@ -0,0 +1,16 @@
+var botonEncriptar = document.querySelector(".bt-encriptar"); 
+var botonDesencriptar = document.querySelector(".bt-desencriptar");
+var botonCopiar = document.querySelector(".bt-copiar");
+
+botonEncriptar.addEventListener("click", function(){
+    encriptar();
+    borrarMensaje();
+});
+botonDesencriptar.addEventListener("click", function(){
+    desencriptar();
+    borrarMensaje();
+});
+    
+botonCopiar.addEventListener("click", function(){
+   copiarTexto();
+});
